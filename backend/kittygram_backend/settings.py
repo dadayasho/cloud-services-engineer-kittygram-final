4fedb7ec5a577faed57b8ab8cca9630df479546f
vboxuser <vboxuser@max.myguest.virtualbox.org>
2025-11-04 23:41:34 +0300

    [d-compose cahnged]

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+#from dotenv import load_dotenv
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,13 +57,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "my_service",
-            "passfile": ".my_pgpass",
-        },
+        "NAME": os.getenv("DB_NAME"),          
+        "USER": os.getenv("DB_USER"),          
+        "PASSWORD": os.getenv("DB_PASSWORD"),  
+        "HOST": os.getenv("DB_HOST"),          
+        "PORT": os.getenv("DB_PORT"),          
     }
 }
 
